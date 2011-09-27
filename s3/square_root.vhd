c0b65709f587695f47ffd5cf3bca510e96819612
Felipe Sampaio <felipe.sampaio@inf.ufrgs.br>
2011-09-27 10:48:40 -0300

    Clock gated latches are needed for the Solution 3

---
+++ s3/square_root.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+
+entity square_root is
+end square_root;
+
+architecture Behavioral of square_root is
+
+begin
+
+
+end Behavioral;
+
